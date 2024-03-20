c25705a04227e295c44f1e30f5157ae69003a4db
Estrelliita <estrellalr216@gmail.com>
2024-03-19 22:00:51 -0600

    Fixed somethings from tester.c to be able to test my methods, but it does not work yet

--- src/tester.c
+++ src/tester.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "tokenizer.h"
 #include "history.h"
+#include "history.c"
 
 /*
     The following is a simple testing script with a single example of input
@@ -21,44 +22,44 @@ int tests_run;
 
 /* Tokenizer test cases */
 static char *test_string_length() {
-    mu_assert("string_length('happy') == 5", string_length("happy") == 5);
+    mu_assert("string_length('happy') == 5", count_tokens("happy") == 5);
     return 0;
 }
 static char *test_is_valid_character() {
-    mu_assert("is_valid_character(' ') == 0", is_valid_character(' ') == 0);
-    mu_assert("is_valid_character('h') == 1", is_valid_character('h') == 1);
+    mu_assert("is_valid_character(' ') == 0", non_space_char(' ') == 0);
+    mu_assert("is_valid_character('h') == 1", non_space_char('h') == 1);
     return 0;
 }
 
 static char *test_find_word_start() {
     char *str = "  happy";
-    mu_assert("find_word_start('  happy') == &str[2]'", find_word_start(str) == &str[2]);
+    mu_assert("find_word_start('  happy') == &str[2]'", token_start(str) == &str[2]);
     return 0;
 }
 
 static char *test_find_word_terminator() {
   char *str = "happy joy", *empty="";
-    mu_assert("find_word_terminator('happy joy') == &str[5]' '", find_word_terminator(str) == &str[5]);
-    mu_assert("find_word_terminator(emptyStr) == empty", find_word_terminator(empty) == empty);
+    mu_assert("find_word_terminator('happy joy') == &str[5]' '", token_terminator(str) == &str[5]);
+    mu_assert("find_word_terminator(emptyStr) == empty", token_terminator(empty) == empty);
     return 0;
 }
 
 static char *test_count_words() {
     char *str = "happy happy joy joy";
-    mu_assert("count_words('happy happy joy joy') == 4", count_words(str) == 4);
+    mu_assert("count_words('happy happy joy joy') == 4", count_tokens(str) == 4);
     return 0;
 }
 
-static char *test_tokenize() {
-    char *str = "happy happy joy joy";
-    char **tokens = tokenize(str);
-    mu_assert("tokens[0] == 'happy'", strcmp(tokens[0], "happy") == 0);
-    mu_assert("tokens[1] == 'happy'", strcmp(tokens[1], "happy") == 0);
-    mu_assert("tokens[2] == 'joy'", strcmp(tokens[2], "joy") == 0);
-    mu_assert("tokens[3] == 'joy'", strcmp(tokens[3], "joy") == 0);
-    free_tokens(tokens);
-    return 0;
-}
+//static char *test_tokenize() {
+//  char *str = "happy happy joy joy";
+//  char **tokens = tokenize(str);
+//  mu_assert("tokens[0] == 'happy'", strcmp(tokens[0], "happy") == 0);
+//  mu_assert("tokens[1] == 'happy'", strcmp(tokens[1], "happy") == 0);
+//  mu_assert("tokens[2] == 'joy'", strcmp(tokens[2], "joy") == 0);
+//  mu_assert("tokens[3] == 'joy'", strcmp(tokens[3], "joy") == 0);
+//  free_tokens(tokens);
+//  return 0;
+//}
 
 /* History test cases */
 static char *test_add_history() {
